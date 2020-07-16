27f312d7c03f41ef9b39d5ca46bc810ca2d4160f
vaddv <vijay.vaddem@gmail.com>
2020-07-16 20:03:15 +0530

    learning function composition in js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+let input = " Javascript ";
+let output = "<div>" + input.trim() + "</div>";
+
+const trim = (str) => str.trim();
+const wrapInDiv = (str) => `<div>${str}</div>`;
+const toLowerCase = (str) => str.toLowerCase();
+
+const result = wrapInDiv(toLowerCase(trim(input)));
+console.log(result);
