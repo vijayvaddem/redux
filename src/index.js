cf750ed53bd3d2a7c27aebff4f08ca36a970eec4
vaddv <vijay.vaddem@gmail.com>
2020-07-26 12:21:08 +0530

    Learning how to dispatch actions from store to add / remove entries from store

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import store from "./store";
+
+store.dispatch({
+  type: "bugAdded",
+  payload: {
+    description: "Bug1",
+  },
+});
+
+store.dispatch({
+  type: "bugRemoved",
+  payload: {
+    id: 1,
+  },
+});
+
+console.log(store.getState());
--- src/reducer.js
+++ src/reducer.js
@@ -1,20 +1,18 @@
-
 let lastId = 0;
 
-export default function reducer (state = [], action) {
-    if(action.type === 'bugAdded') {
-        return [
-            ...state,
-            {
-                id = ++lastId,
-                description = action.payload.description,
-                resolved: false
-            } 
-        ];
-    }
-    else if (action.type === 'bugRemoved') {
-        return state.filter(bug => bug.id !== action.payload.id);
-    }
+export default function reducer(state = [], action) {
+  if (action.type === "bugAdded") {
+    return [
+      ...state,
+      {
+        id: ++lastId,
+        description: action.payload.description,
+        resolved: false,
+      },
+    ];
+  } else if (action.type === "bugRemoved") {
+    return state.filter((bug) => bug.id !== action.payload.id);
+  }
 
-    return state;
+  return state;
 }
