dac3642a4b7b6f150cf42f9180940969ac60c8a3
vaddv <vijay.vaddem@gmail.com>
2020-07-28 23:36:40 +0530

    Added custom store

--- src/index.js
+++ src/index.js
@@ -1,14 +1,3 @@
-import store from "./store";
-import { bugAdded, bugResolved } from "./actions";
-import * as actionTypes from "./actionTypes";
-
-const unsubscribe = store.subscribe(() => {
-  console.log("Store changed", store.getState());
-});
-
-store.dispatch(bugAdded("Bug 1"));
-store.dispatch(bugResolved(1));
-
-unsubscribe();
-
-console.log(store.getState());
+import store from "./customStore";
+store.state = 2;
+console.log(store);
