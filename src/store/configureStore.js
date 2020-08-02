45b53000ffc337e38f3a425f13db0527e6761c78
vaddv <vijay.vaddem@gmail.com>
2020-08-02 18:56:40 +0530

    Refactored configureStore to use @reduxjs/toolkit API

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,8 +1,8 @@
-import { createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
+
 import reducer from "./bugs";
 import { devToolsEnhancer } from "redux-devtools-extension";
 
-export default function configureStore() {
-  const store = createStore(reducer, devToolsEnhancer({ trace: true }));
-  return store;
+export default function () {
+  return configureStore({ reducer });
 }
