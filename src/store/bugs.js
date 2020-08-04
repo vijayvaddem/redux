db2ba691d5c38dd62a012a62343ade864a12db52
vaddv <vijay.vaddem@gmail.com>
2020-08-04 23:19:29 +0530

    Implemented slicing approach

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,33 +1,24 @@
-import { createAction, createReducer, createSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
-createSlice({
+let lastId = 0;
+
+const slice = createSlice({
   name: "bugs",
   initialState: [],
   reducers: {
-    bugAdded(state, action),
-    bugResolved,
+    bugAdded: (bugs, action) => {
+      bugs.push({
+        id: ++lastId,
+        description: action.payload.description,
+        resolved: false,
+      });
+    },
+    bugResolved: (bugs, action) => {
+      const index = bugs.findIndex((bug) => bug.id === action.payload.id);
+      bugs[index].resolved = true;
+    },
   },
 });
 
-//Action creators
-export const bugAdded = createAction("bugAdded");
-export const bugResolved = createAction("bugResolved");
-export const bugRemoved = createAction("bugRemoved");
-
-//Reducer
-let lastId = 0;
-
-export default createReducer([], {
-  [bugAdded.type]: (bugs, action) => {
-    bugs.push({
-      id: ++lastId,
-      description: action.payload.description,
-      resolved: false,
-    });
-  },
-
-  [bugResolved.type]: (bugs, action) => {
-    const index = bugs.findIndex((bug) => bug.id === action.payload.id);
-    bugs[index].resolved = true;
-  },
-});
+export default slice.reducer;
+export const { bugAdded, bugResolved } = slice.actions;
